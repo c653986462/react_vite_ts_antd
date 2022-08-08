3b970c017703905f4d956cebee7a645d0e6530d2
Cmy20201028 <mingyu.chi@shiduai.com>
2022-08-08 11:20:00 +0800

    增加postcss适配

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import "antd/dist/antd.css";
 import App from "./app";
+import 'amfe-flexible/index.js'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
